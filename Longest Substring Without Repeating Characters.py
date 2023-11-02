c9e6d6d46436307b72a1c4e25121ae731b473ec7
nakulupadhyay <142798383+nakulupadhyay@users.noreply.github.com>
2023-11-02 15:47:54 +0530

    Longest Substring

---
+++ Longest Substring Without Repeating Characters.py
@@ -0,0 +1,12 @@
+seen = {}
+l = 0
+length = 0
+for r in range(len(s)):
+                    
+                    if char in seen and seen[char] >= l:
+                                
+                    else:
+                                    length = max(length, r - l + 1)
+                                
+
+print(length)                           
