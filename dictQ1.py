12244eee6d0eb098f3824809c09161af68dfd183
nakulupadhyay <142798383+nakulupadhyay@users.noreply.github.com>
2023-11-22 19:04:35 +0530

    using dictionary

---
+++ dictQ1.py
@@ -0,0 +1,13 @@
+info1={'name':'jay',
+       'rollno':38,
+       'college':'gla university',
+       'cpi':8.9}
+
+new_info={'name':'naman',
+          'mobile':7894561234}
+
+for i in new_info:
+    info1[i]=new_info[i]
+
+
+print(info1)
