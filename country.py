378835846698f7dbbcfbb85749e6a36b01a2a9e9
nakulupadhyay <142798383+nakulupadhyay@users.noreply.github.com>
2023-11-22 19:25:16 +0530

    function

---
+++ country.py
@@ -0,0 +1,10 @@
+
+def python_def_keyword(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+python_def_keyword(name='Alice', 
+                   age=25, city='New York')
+python_def_keyword(country='Canada', 
+                   language='French', population=38000000)
+python_def_keyword(color='blue', 
+                   shape='square', size='large', material='wood')
